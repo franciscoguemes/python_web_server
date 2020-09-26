7c1c463c6a063e2ddcda9e19d660ce3dc303491b
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-09-26 02:09:26 +0200

    Comment the part of the thread

--- gui/ProgressBarWindow.py
+++ gui/ProgressBarWindow.py
@@ -17,11 +17,8 @@ class ProgressBarWindow(Toplevel):
         Toplevel.__init__(self, master, height=self.DEFAULT_HEIGHT, width=self.DEFAULT_WIDTH)
         self.resizable(0,0)
         self.title("Scanning opened ports...")
-        # ports_window = tkinter.Toplevel(self.master)
-        # ports_window.geometry("500x300")
-        # ports_window.resizable(0, 0)
-        # ports_window.title("Ports in use")
         self.__add_components()
+        print("After adding components...")
 
         # TODO: Change this, go for the approach of creating a class for the thread...
         # http://blog.acipo.com/python-threading-progress-indicators/
@@ -29,26 +26,29 @@ class ProgressBarWindow(Toplevel):
         # https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
         # https://realpython.com/intro-to-python-threading/#starting-a-thread
 
-        self.__opened_ports = None
-        port_scanner = PortScanner()
-        thread = threading.Thread(target=port_scanner.scan)
-        print("Before thread start")
-        thread.start()
-        print("After thread start")
-        thread.join()
-        print("After thread join...")
-        while thread.is_alive():
-            print("Checking thread is alive...")
-            # Get thread progress
-            progress = port_scanner.get_progress()
-            # Set progress in the bar...
-            self.__progressbar.step(progress)
-            # sleep
-            time.sleep(0.1)
+        # self.__opened_ports = None
+        # port_scanner = PortScanner()
+        # thread = threading.Thread(target=port_scanner.scan)
+        # print("Before thread start")
+        # thread.start()
+        # print("After thread start")
+        # while not port_scanner.is_scan_finished():
+        #     print("Checking thread is alive...")
+        #     # Get thread progress
+        #     progress = port_scanner.get_progress()
+        #     # Set progress in the bar...
+        #     self.__progressbar['value'] = progress
+        #     # sleep
+        #     time.sleep(0.1)
+        #
+        # print("Before thread...")
+        # thread.join()
+        # print("After thread join...")
+        #
+        # print("At the very end...")
 
     def __add_components(self):
         self.__progressbar = ttk.Progressbar(self, orient="horizontal", length=self.DEFAULT_WIDTH, mode="determinate")
-        self.__progressbar.start(0)
         self.__progressbar.place(x=25, y=25, width=200, height=20)
 
         # TODO: Center the button...
@@ -59,5 +59,8 @@ class ProgressBarWindow(Toplevel):
         # TODO: Delete this message once the functionality is working
         messagebox.showwarning("Cancel scan", "Scan cancelled")
 
-    def get_opened_ports(self):
-        return self.__opened_ports
+    def set_progress(self, progress):
+        self.__progressbar['value'] = progress
+
+    # def get_opened_ports(self):
+    #     return self.__opened_ports
