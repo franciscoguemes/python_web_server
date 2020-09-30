fa544c78122fe5c03de6f98d8a3bd9f3f0b708fd
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-09-30 19:17:16 +0200

    Cleanup code in PortScanner.py.

--- utils/PortScanner.py
+++ utils/PortScanner.py
@@ -16,16 +16,6 @@ class PortScanner:
         self.__index = 0
         self.__cancelled = False
 
-    # def get_opened_ports(self):
-    #     list_of_ports = ""
-    #     for port in range(self.RANGE_START, self.RANGE_END):
-    #         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #         result = sock.connect_ex((self.DEFAULT_SERVER, port))
-    #         if result == 0:
-    #             list_of_ports += "Port {: >5}: 	 Open\n".format(port)
-    #         sock.close()
-    #     return list_of_ports;
-
     def is_scan_finished(self):
         """The scan is finished either because it terminated (in a natural way) or because it was cancelled (abnormal
         termination)
