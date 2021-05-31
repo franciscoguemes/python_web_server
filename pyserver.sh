775ee5b0a303d8d757ec11120ebab88b056b6e26
Francisco Güemes <francisco.gueemes@uniscon.com>
2021-05-31 12:50:03 +0200

    Modify pyserver.sh

---
+++ pyserver.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PYSERVER_DIRECTORY=/home/francisco/git/Francisco/github/python_web_server
+CONFIG_DIRECTORY=/home/francisco/.config/DbWS
+
+cd $PYSERVER_DIRECTORY
+./pyserver.py
