6eaf323151f768e174c7c8b677c980a96a3ea247
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2022-03-18 19:30:20 +0200

    Fix script for starting pyserver

--- pyserver.sh
+++ pyserver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PYSERVER_DIRECTORY=/home/francisco/git/Francisco/github/python_web_server
+PYSERVER_DIRECTORY=/home/francisco/git/francisco/github/python_web_server
 CONFIG_DIRECTORY=/home/francisco/.config/DbWS
 
 cd $PYSERVER_DIRECTORY
